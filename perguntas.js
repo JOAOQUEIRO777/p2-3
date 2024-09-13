b8129a6f115e861e0630bd16185c1c3a9ca5c373
JOAOQUEIRO777 <159548926+JOAOQUEIRO777@users.noreply.github.com>
2024-09-13 08:27:42 -0300

    h

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Carros',
+    'Qual o carro mais rápido do mundo?',
+    'Koenigsegg Jesko Absolut,cerca de 531 km.'
 )
 
 criaCartao(
